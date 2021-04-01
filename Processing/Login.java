fcdcc2dd069a3854ea63121f852113a8659d33f7
iceE-1591 <icee1591@gmail.com>
2021-04-01 23:00:05 +0900

    MainWindowとLoginのコントローラを作成。 Login完了時の画面遷移処理を追加

---
+++ Processing/Login.java
@@ -0,0 +1,57 @@
+package main;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import java.net.URI;
+
+
+public class Login {
+    @FXML
+    private TextField email;
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private StackPane window;
+
+    @FXML
+    private Button loginButton;
+
+    public static MainWindow mw;
+
+    public void setMW(MainWindow mw) {
+        this.mw = mw;
+    }
+
+    // login処理
+    @FXML
+    void onLoginClick(ActionEvent event) throws Exception {
+        // login完了したら
+        if (true) {
+            mw.loginCompleted();
+        }
+    }
+
+    @FXML
+    void createAccount(ActionEvent event) {
+
+    }
+
+    @FXML
+    void forgotPassword(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {}
+}
