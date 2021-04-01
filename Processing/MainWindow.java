486c8fd72e6bc271bdeae74d47adbd007f54525a
iceE-1591 <icee1591@gmail.com>
2021-04-02 00:17:38 +0900

    現在ある全てのfxmlをソフトウェア実行時に読み込み、mainWindowにaddする処理を追加しました。＆Parts変数をstaticにしました

--- Processing/MainWindow.java
+++ Processing/MainWindow.java
@@ -1,46 +1,61 @@
 package main;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.StackPane;
 
 public class MainWindow {
 
     @FXML
-    private StackPane mainWindow;
+    protected StackPane mainWindow;
 
     @FXML
-    private StackPane addParts;
+    protected StackPane addParts;
 
     @FXML
-    private StackPane menuBar;
+    protected StackPane menuBar;
 
-    private StackPane loginParts;   // login系統Pane
-    private StackPane menuParts;    // menu系統Pane
-    private StackPane menuBarParts; // menuBar系統Pane
+    public static StackPane loginParts, createAccountParts;    // login系統Pane
+    public static StackPane menuParts, addAnswerParts,
+                            addProblemParts, editProblemParts; // menu系統Pane
+    public static StackPane menuBarParts;                      // menuBar系統Pane
+    public static StackPane afterAddProblemParts;              // その他Pane
 
-    public void loginCompleted() throws Exception {
-        mainWindow.getChildren().remove(loginParts);
-        menuBarParts.setVisible(true);
-        menuParts.setVisible(true);
-    }
-
-    private void loadParts() throws Exception  {
+    protected void loadParts() throws Exception  {
         // load fxml
-        loginParts = (StackPane) FXMLLoader.load(getClass().getResource("createAccount/createAccount.fxml"));
-        menuBarParts = (StackPane) FXMLLoader.load(getClass().getResource("menuBar/menuBar.fxml"));
-        menuParts = (StackPane) FXMLLoader.load(getClass().getResource("mainMenu/mainMenu.fxml"));
-
-        // add login parts
-        mainWindow.getChildren().add(loginParts);
+        loginParts           = (StackPane) FXMLLoader.load(getClass().getResource("login/login.fxml"));
+        createAccountParts   = (StackPane) FXMLLoader.load(getClass().getResource("createAccount/createAccount.fxml"));
+        menuBarParts         = (StackPane) FXMLLoader.load(getClass().getResource("menuBar/menuBar.fxml"));
+        menuParts            = (StackPane) FXMLLoader.load(getClass().getResource("mainMenu/mainMenu.fxml"));
+        addAnswerParts       = (StackPane) FXMLLoader.load(getClass().getResource("addAnswer/addAnswer.fxml"));
+        addProblemParts      = (StackPane) FXMLLoader.load(getClass().getResource("addProblem/addProblem.fxml"));
+        afterAddProblemParts = (StackPane) FXMLLoader.load(getClass().getResource("afterAddProblem/afterAddProblem.fxml"));
+        editProblemParts     = (StackPane) FXMLLoader.load(getClass().getResource("editProblem1/editProblem1.fxml"));
 
         // visible settings
-        menuBarParts.setVisible(false);
-        menuParts.setVisible(false);
+        loginParts          .setVisible(true);
+        createAccountParts  .setVisible(false);
+        menuBarParts        .setVisible(false);
+        menuParts           .setVisible(false);
+        addAnswerParts      .setVisible(false);
+        addProblemParts     .setVisible(false);
+        afterAddProblemParts.setVisible(false);
+        editProblemParts    .setVisible(false);
 
         // add other parts
-        menuBar.getChildren().add(menuBarParts);
+        mainWindow.getChildren().add(loginParts);
+        mainWindow.getChildren().add(createAccountParts);
+
         addParts.getChildren().add(menuParts);
+        addParts.getChildren().add(addAnswerParts);
+        addParts.getChildren().add(addProblemParts);
+        addParts.getChildren().add(afterAddProblemParts);
+        addParts.getChildren().add(editProblemParts);
+
+        menuBar.getChildren().add(menuBarParts);
+
     }
 
     @FXML
