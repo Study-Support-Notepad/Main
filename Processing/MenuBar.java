44cc29c6b7f0c2b0dcbcc42d2d762ec552a9d7e4
iceE-1591 <icee1591@gmail.com>
2021-04-05 03:49:55 +0900

    menuBarの各要素にtooltip処理を追加

--- Processing/MenuBar.java
+++ Processing/MenuBar.java
@@ -1,12 +1,13 @@
 package main;
 
-import java.awt.*;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Tooltip;
 
 public class MenuBar {
 
@@ -16,11 +17,24 @@ public class MenuBar {
     @FXML
     private URL location;
 
+    @FXML
+    private Button tooltipSettings;
+
+    @FXML
+    private Button tooltipLogout;
+
+    @FXML
+    private Button tooltipHelp;
+
     private final String helpUrl = "https://github.com/Study-Support-Notepad";
 
     @FXML
     void onBackToMenu(ActionEvent event) {
-
+        if (!MainWindow.menuParts.isVisible()) {
+            MainWindow.allFalseVisible();
+            MainWindow.menuBarParts.setVisible(true);
+            MainWindow.menuParts.setVisible(true);
+        }
     }
 
     @FXML
@@ -30,6 +44,8 @@ public class MenuBar {
 
     @FXML
     void onLogout(ActionEvent event) {
+        MainWindow.allFalseVisible();
+        MainWindow.loginParts.setVisible(true);
 
     }
 
@@ -37,8 +53,18 @@ public class MenuBar {
     void onSettings(ActionEvent event) {
     }
 
+    private void setTooltip() {
+        Tooltip tpSettings = new Tooltip("設定");
+        Tooltip tpLogout = new Tooltip("ログアウト");
+        Tooltip tpHelp = new Tooltip("ヘルプ");
+        tooltipSettings.setTooltip(tpSettings);
+        tooltipLogout.setTooltip(tpLogout);
+        tooltipHelp.setTooltip(tpHelp);
+    }
+
     @FXML
     void initialize() {
+        setTooltip();
 
     }
 }
