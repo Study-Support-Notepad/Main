7668d424f76f232e62f82261d73002ae924946b1
iceE-1591 <icee1591@gmail.com>
2021-04-07 21:54:51 +0900

    PreLoaderを表示されるようにしました

--- Processing/Main.java
+++ Processing/Main.java
@@ -6,27 +6,39 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
-
-import java.sql.Connection;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
+    public static Stage primaryStage;
+    public static Stage primaryStage2;
+
     public static void main(String[] args) throws Exception{
-        Connection con = DataBase.getConnection();
-        con.close();
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage)  throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("mainWindow/mainWindow.fxml"));
-        Parent root = (Parent)loader.load();
-        primaryStage.setScene(new Scene(root, 740, 458));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("starting/starting.fxml"));
+        Parent root = loader.load();
+        primaryStage.setScene(new Scene(root, 500, 300));
         Image icon = new Image( "main/images/logo.png" );
         primaryStage.getIcons().add(icon);
-        primaryStage.setResizable(true);
-        primaryStage.setMinHeight(400);
-        primaryStage.setMinWidth(600);
+        primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.show();
+        this.primaryStage = primaryStage;
+
+
+        FXMLLoader loader2 = new FXMLLoader(getClass().getResource("mainWindow/mainWindow.fxml"));
+        Parent root2 = loader2.load();
+        Stage primaryStage2 = new Stage();
+        primaryStage2.setScene(new Scene(root2, 740, 458));
+        primaryStage2.setResizable(true);
+        Image icon2 = new Image("main/images/logo.png");
+        primaryStage2.getIcons().add(icon2);
+        primaryStage2.setMinHeight(400);
+        primaryStage2.setMinWidth(600);
+        this.primaryStage2 = primaryStage2;
+        new FirstRun().start();
     }
 }
