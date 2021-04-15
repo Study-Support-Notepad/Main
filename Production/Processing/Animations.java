c61a41d893d9f80b3e0780b28103729c240b91a0
iceE-1591 <icee1591@gmail.com>
2021-04-15 22:25:57 +0900

    Starting画面の回転アニメーションをAnimationsに追加

--- Production/Processing/Animations.java
+++ Production/Processing/Animations.java
@@ -10,4 +10,12 @@ import javafx.util.Duration;
 public class Animations {
 
 
+
+    public static void startingRotate(StackPane node) {
+        RotateTransition animation = new RotateTransition(Duration.seconds(2.25), node);
+        animation.setByAngle(360);
+        animation.setCycleCount(TranslateTransition.INDEFINITE);
+        animation.setInterpolator(Interpolator.LINEAR);
+        animation.play();
+    }
 }
