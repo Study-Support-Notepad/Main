704c97b651d6384deaf22deab1fedec8715ed983
iceE-1591 <icee1591@gmail.com>
2021-04-15 22:25:16 +0900

    Animation処理をまとめるクラスを作成

---
+++ Production/Processing/Animations.java
@@ -0,0 +1,13 @@
+package main;
+
+import animatefx.animation.FadeOutLeft;
+import javafx.animation.Interpolator;
+import javafx.animation.RotateTransition;
+import javafx.animation.TranslateTransition;
+import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
+
+public class Animations {
+
+
+}
