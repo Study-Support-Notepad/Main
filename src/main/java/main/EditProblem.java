dc8a7e3a5512e745044759756b01e5b214f5ea87
iceE-1591 <icee1591@gmail.com>
2021-04-18 00:41:07 +0900

    EditProblemのinitializeを削除

--- src/main/java/main/EditProblem.java
+++ src/main/java/main/EditProblem.java
@@ -36,8 +36,5 @@ public class EditProblem {
 
     @FXML
     void initialize() {
-        assert searchTitle != null : "fx:id=\"searchTitle\" was not injected: check your FXML file 'editProblem1.fxml'.";
-        assert selectTag != null : "fx:id=\"selectTag\" was not injected: check your FXML file 'editProblem1.fxml'.";
-        assert problems != null : "fx:id=\"problems\" was not injected: check your FXML file 'editProblem1.fxml'.";
     }
 }
