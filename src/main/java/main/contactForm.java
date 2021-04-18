6e83824d1423d85caddbfa1a7248369f7497af0c
iceE-1591 <icee1591@gmail.com>
2021-04-18 12:29:18 +0900

    contactFormコントローラークラスを作成

---
+++ src/main/java/main/contactForm.java
@@ -0,0 +1,18 @@
+package main;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
+public class contactForm {
+
+    @FXML
+    private Button onMenuOrAddProblemMove;
+
+    @FXML
+    private TextArea memo;
+
+    @FXML
+    void initialize() {
+    }
+}
