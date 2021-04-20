7b3f01d4a0d271c5ebf61e44ed043659864cb6a7
iceE-1591 <icee1591@gmail.com>
2021-04-20 20:49:01 +0900

    CreateAccountコントローラーにwarning時のアニメーションを追加

--- src/main/java/main/CreateAccount.java
+++ src/main/java/main/CreateAccount.java
@@ -48,22 +48,28 @@ public class CreateAccount {
                                 なければアカウントの作成
                             } else {
                                 warningTxt.setText("既にアカウントが存在しています");
+                                Animations.failureLoginSystem(createButton);
                             }
                              */
                         } else {
                             warningTxt.setText("利用規約に同意してください");
+                            Animations.failureLoginSystem(createButton);
                         }
                     } else {
                         warningTxt.setText("passwordが一致しません");
+                        Animations.failureLoginSystem(createButton);
                     }
                 } else {
                     warningTxt.setText("passwordを正しく入力してください");
+                    Animations.failureLoginSystem(createButton);
                 }
             } else {
                 warningTxt.setText("名前を正しく入力してください");
+                Animations.failureLoginSystem(createButton);
             }
         } else {
             warningTxt.setText("EmailAddressを正しく入力してください");
+            Animations.failureLoginSystem(createButton);
         }
     }
 
