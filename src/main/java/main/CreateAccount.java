94282c8adb1ca208738f73d6fe38f70fe607f79f
iceE-1591 <icee1591@gmail.com>
2021-04-25 20:38:20 +0900

    RegexesクラスからCheckメソッドを呼ぶように変更

--- src/main/java/main/CreateAccount.java
+++ src/main/java/main/CreateAccount.java
@@ -39,9 +39,9 @@ public class CreateAccount {
 
     @FXML
     void onCreateAccount(ActionEvent event) throws Exception { // 空白がないかどうか パスワードの文字列 既に存在しているアカウントかどうか
-        if (checkEmail(email.getText())) {
-            if (checkName(userName.getText())) {
-                if (checkPassword(password.getText())) {
+        if (Regexes.checkEmail(email.getText())) {
+            if (Regexes.checkName(userName.getText())) {
+                if (Regexes.checkPassword(password.getText())) {
                     if (password.getText().equals(passwordCheck.getText())) {
                         if (termsCheck.isSelected()) {
                             /* if (アカウントがあるかどうか) {
@@ -72,24 +72,6 @@ public class CreateAccount {
             warningTxt.setText("EmailAddressを正しく入力してください");
             Animations.failureLoginSystem(createButton);
         }
-    }
-
-    private boolean checkEmail(String txt) {
-        String pattern = "[a-zA-Z0-9.!#$%&’*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*";
-        Pattern p = Pattern.compile(pattern);
-        return p.matcher(txt).matches();
-    }
-
-    private boolean checkPassword(String txt) {
-        String pattern = "[0-9a-zA-Z!?]{8,50}+";
-        Pattern p = Pattern.compile(pattern);
-        return p.matcher(txt).matches();
-    }
-
-    private boolean checkName(String txt)  {
-        String pattern = "[0-9a-zA-Z]{3,20}+";
-        Pattern p = Pattern.compile(pattern);
-        return p.matcher(txt).matches();
     }
 
     @FXML
