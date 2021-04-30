c1ea31b7a24edcc9ffa9634cc8f44ed45f580270
iceE-1591 <icee1591@gmail.com>
2021-05-01 00:53:37 +0900

    file.fxmlコントローラークラスの追加

---
+++ src/main/java/main/File.java
@@ -0,0 +1,32 @@
+package main;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+
+public class File {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Pane Parts;
+
+    @FXML
+    private Label name;
+
+    @FXML
+    void onEdit(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+    }
+}
